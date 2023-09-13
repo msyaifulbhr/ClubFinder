a069047279cd251f1d373e07fd8393c0e53410ec
msyaifulbhr <syaifulyy@gmail.com>
2023-09-13 17:33:19 +0700

    module

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,3 +1,5 @@
+import DataSource from '../data/data-source.js';
+
 const main = () => {
   const searchElement = document.querySelector('#searchElement');
   const buttonSearchElement = document.querySelector('#searchButtonElement');
@@ -14,25 +16,30 @@ const main = () => {
 
   const renderResult = results => {
     clubListElement.innerHTML = '';
-    results.forEach(function (club) {
-      const { name, fanArt, description } = club;
 
+    results.forEach(club => {
+      const {name, fanArt, description} = club;
       const clubElement = document.createElement('div');
       clubElement.setAttribute('class', 'club');
 
-      clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-          <div class="club-info">
+      clubElement.innerHTML = `
+        <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+        <div class="club-info">
           <h2>${name}</h2>
           <p>${description}</p>
-          </div>`;
+        </div>
+      `;
+
       clubListElement.appendChild(clubElement);
     });
   };
 
   const fallbackResult = message => {
     clubListElement.innerHTML = '';
-    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
+    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   };
 
   buttonSearchElement.addEventListener('click', onButtonSearchClicked);
 };
+
+export default main;
