79ee004782d5d1454e75a9288294584b937d92bf
msyaifulbhr <syaifulyy@gmail.com>
2023-09-24 20:02:29 +0700

    refactor fetch

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,14 +1,14 @@
-import clubs from './clubs.js';
-
 class DataSource {
   static searchClub(keyword) {
-    return new Promise((resolve, reject) => {
-      const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-      if (filteredClubs.length) {
-        resolve(filteredClubs);
+    return fetch(`https://sports-api.dicoding.dev/teams/search?t=${keyword}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(responseJson => {
+      if (responseJson.teams) {
+        return Promise.resolve(responseJson.teams);
       } else {
-        reject(`${keyword} is not found`);
+        return Promise.reject(`${keyword} is not found`);
       }
     });
   }
